54c752c8e918147e0ed92004079c383826630250
michaelcesar <michaelcesarflopesgit@gmail.com>
2024-10-06 20:41:58 -0300

    first commit

---
+++ src/app/links/page.tsx
@@ -0,0 +1,9 @@
+import styles from './links.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.wrapper}>
+      <section className={styles.content}></section>
+    </main>
+  );
+}
