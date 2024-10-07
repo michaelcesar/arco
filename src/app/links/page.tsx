c28f966849e3f7bfa13da218f0d6baac27f5e29e
michaelcesar <michaelcesarflopesgit@gmail.com>
2024-10-06 22:17:37 -0300

    feat: adjustments

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,9 +1,12 @@
 import styles from './links.module.css';
+import Logo from '@/components/atoms/logo';
 
 export default function Home() {
   return (
     <main className={styles.wrapper}>
-      <section className={styles.content}></section>
+      <section className={styles.content}>
+        <Logo url="/images/logo_text.svg" width={200} height={140}></Logo>
+      </section>
     </main>
   );
 }
