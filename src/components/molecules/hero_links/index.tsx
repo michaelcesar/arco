2678409fee59fac469818891b48cc97f98842634
michaelcesar <michaelcesarflopesgit@gmail.com>
2024-10-07 07:59:38 -0300

    feat: adjustments

--- src/components/molecules/hero_links/index.tsx
+++ src/components/molecules/hero_links/index.tsx
@@ -5,9 +5,9 @@ export default function HeroLinks() {
     <aside className={styles.wrapper}>
       <p className={styles.text}>
         <span className={styles.highlighted}>Transforme</span> sua presença
-        digital e <span className={styles.highlighted}>impulsione</span> seu{' '}
-        <span className={styles.highlighted}>negócio</span> com soluções
-        digitais.
+        digital <br></br>e{' '}
+        <span className={styles.highlighted}>impulsione</span> seu{' '}
+        <span className={styles.highlighted}>negócio</span>.
       </p>
     </aside>
   );
